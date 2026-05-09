4557a6febc96bbe15df566c6f3f4f39accde3eda
AlessioP04 <paparcurioalessio@gmail.com>
2026-05-10 00:21:39 +0200

    Aggiunti percorsi alternativi basati sul peso e counter per il numero di viti da aggiungere/rimuovere

--- matlab/IndustrialObject.m
+++ matlab/IndustrialObject.m
@@ -1,41 +1,50 @@
 classdef IndustrialObject < handle
     
     properties
-        Type
-        Weight
-        Graphic 
-        State
+        type
+        weight
+        graphic 
+        state
+        shouldDelete = false;
     end
     
     methods
         
-        function obj = IndustrialObject(type, weight, graphic)           
-            obj.Type = type;
-            obj.Weight = weight;
-            obj.Graphic = graphic; 
-            obj.State = "Entered";
+        function obj = IndustrialObject(weight, graphic)           
+            obj.type = "NotClassified";
+            obj.state
+            obj.weight = weight;
+            obj.graphic = graphic; 
         end
         
         %% Movement functions
-        function Move(obj, dx, dy)            
-            p = obj.Graphic.Position;           
+        function move(obj, dx, dy)            
+            p = obj.graphic.Position;           
             p(1) = p(1) + dx;
             p(2) = p(2) + dy;            
-            obj.Graphic.Position = p;            
+            obj.graphic.Position = p;            
         end
         
         %% Getters and setters
         
-        function x = GetX(obj)           
-            x = obj.Graphic.Position(1);           
+        function x = getX(obj)           
+            x = obj.graphic.Position(1);           
         end
 
-        function w = GetWeight(obj)
-            w = obj.Weight;
+        function y = getY(obj)           
+            y = obj.graphic.Position(2);           
+        end
+
+        function w = getWeight(obj)
+            w = obj.weight;
         end
         
-        function SetState(obj, state)
-            obj.State = state;
+        function setState(obj, newState)
+            obj.state = newState;
+        end
+
+        function setType(obj, newType)
+            obj.type = newType;
         end
     end
     
